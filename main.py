84653de88e998ea93ce57c3f9a0e9e032ae88ef1
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-11-18 23:51:29 +0200

    import modules & load images

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+import os
+import time
+import random
+
+# load images
+RED_SPACE_SHIP = pygame.image.load(
+    os.path.join("assets", "pixel_ship_red_small.png"))
+GREEN_SPACE_SHIP = pygame.image.load(
+    os.path.join("assets", "pixel_ship_green_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(
+    os.path.join("assets", "pixel_ship_blue_small.png"))
+
+# player ship
+YELLOW_SPACE_SHIP = pygame.image.load(
+    os.path.join("assets", "pixel_ship_yellow.png"))
