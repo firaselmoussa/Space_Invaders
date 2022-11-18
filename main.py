633239fe9429187e4a16f59433efc84b12b68541
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-11-19 01:11:08 +0200

    implemented Player class

--- main.py
+++ main.py
@@ -48,7 +48,16 @@ class Ship:
         self.cool_down_counter = 0
 
     def draw(self, window):
-        pygame.draw.rect(window, (255, 0, 0), (self.x, self.y, 50, 50))
+        window.blit(self.ship_img, (self.x, self.y))
+
+
+class Player(Ship):
+    def __init__(self, x, y, health=100):
+        super().__init__(x, y, health)
+        self.ship_img = YELLOW_SPACE_SHIP
+        self.laser_img = YELLOW_LASER
+        self.mask = pygame.mask.from_surface(self.ship_img)
+        self.max_heath = health
 
 
 def main():
@@ -60,7 +69,7 @@ def main():
 
     player_vel = 5
 
-    ship = Ship(300, 650)
+    player = Player(300, 650)
 
     clock = pygame.time.Clock()
 
@@ -73,7 +82,7 @@ def main():
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
 
-        ship.draw(WIN)
+        player.draw(WIN)
         pygame.display.update()
 
     while run:
@@ -86,14 +95,14 @@ def main():
 
         # move player
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and ship.x - player_vel > 0:
-            ship.x -= player_vel
-        if keys[pygame.K_RIGHT] and ship.x + player_vel + 50 < WIDTH:
-            ship.x += player_vel
-        if keys[pygame.K_DOWN] and ship.y + player_vel + 50 < HEIGHT:
-            ship.y += player_vel
-        if keys[pygame.K_UP] and ship.y - player_vel > 0:
-            ship.y -= player_vel
+        if keys[pygame.K_LEFT] and player.x - player_vel > 0:
+            player.x -= player_vel
+        if keys[pygame.K_RIGHT] and player.x + player_vel + 50 < WIDTH:
+            player.x += player_vel
+        if keys[pygame.K_DOWN] and player.y + player_vel + 50 < HEIGHT:
+            player.y += player_vel
+        if keys[pygame.K_UP] and player.y - player_vel > 0:
+            player.y -= player_vel
 
 
 main()
