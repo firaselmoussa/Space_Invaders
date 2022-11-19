13c788a2a09a90747b5ba48a51aa215ae3997a8b
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-11-19 03:18:09 +0200

    optimised shooting process

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ class Ship:
 
     def shoot(self):
         if self.cool_down_counter == 0:
-            laser = Laser(self.x, self.y, self.laser_img)
+            laser = Laser(self.x, self.y-15, self.laser_img)
             self.lasers.append(laser)
             self.cool_down_counter = 1
 
@@ -146,6 +146,12 @@ class Enemy(Ship):
         super().__init__(x, y, health)
         self.ship_img, self.laser_img = self.COLOR_MAP[color]
         self.mask = pygame.mask.from_surface(self.ship_img)
+
+    def shoot(self):
+        if self.cool_down_counter == 0:
+            laser = Laser(self.x-10, self.y, self.laser_img)
+            self.lasers.append(laser)
+            self.cool_down_counter = 1
 
     def move(self, vel):
         self.y += vel
@@ -236,6 +242,10 @@ def main():
         for enemy in enemies[:]:
             enemy.move(enemy_vel)
             enemy.move_lasers(laser_vel, player)
+
+            if random.randrange(0, 2*FPS) == 1:
+                enemy.shoot()
+
             if enemy.y + enemy.get_height() > HEIGHT:
                 lives -= 1
                 enemies.remove(enemy)
